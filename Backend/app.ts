ffa56a4e89f2a851d933d34f09dd31386737997f
Nijas ma <nijazz1@gmail.com>
2025-01-20 14:10:42 +0530

    port updated

--- Backend/app.ts
+++ Backend/app.ts
@@ -4,8 +4,8 @@ import cors from 'cors'
 
 import router from "./Routes/route";
 
-dotenv.config();
 const app = express();
+dotenv.config();
 
 app.use(cors({
     origin:["https://ocr-adhar.vercel.app/","http://localhost:5173"],
@@ -14,6 +14,6 @@ app.use(cors({
 app.use('/',router)
 const port = process.env.PORT || 8000;
   
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
